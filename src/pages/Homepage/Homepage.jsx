5c123d7d72817d7049894279a0d5aac498bff553
pizzaYami <cyj9407@naver.com>
2024-04-08 00:31:57 +0900

    Feat: Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import Banner from "./components/Banner/Banner";
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
 }
 
 export default Homepage;
+
+/**
+ * 배너 => api로 popoula 영화를 들고와서 첫번째 아이템을 보여주자
+ * popular movie
+ * top rated movie
+ * upcoming movie
+ */
